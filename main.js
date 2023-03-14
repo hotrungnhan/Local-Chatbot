42e2ff85bf2f516a3955e16415897c7561e9c1b3
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2023-03-14 16:11:43 +0700

    update redis

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const state = new State();
 
 login({ appState: JSON.parse(CREDENTIAL) }, {}, (err, api) => {
     if (err) return console.error(err);
-    api.listen((err, message) => {
+    api.listen(async (err, message) => {
         if (err) {
             console.log(err);
             return;
@@ -25,9 +25,9 @@ login({ appState: JSON.parse(CREDENTIAL) }, {}, (err, api) => {
         console.log(message.threadID, " : ", state.getBot(message.threadID))
         if (message.body == 'bot on') {
             const m = `Bot is on now.`
-            state.addConversation(message.threadID, "system", m);
-            state.setBot(message.threadID, true)
-            api.sendMessage(m, message.threadID);
+            await state.addConversation(message.threadID, "system", m);
+            await state.setBot(message.threadID, true)
+            await api.sendMessage(m, message.threadID);
             return
         }
 
@@ -38,38 +38,38 @@ login({ appState: JSON.parse(CREDENTIAL) }, {}, (err, api) => {
 
         if (message.body == 'bot off') {
             const m = `Bot is off.`
-            state.addConversation(message.threadID, "system", m);
-            state.setBot(message.threadID, false)
-            api.sendMessage(m, message.threadID);
+            await state.addConversation(message.threadID, "system", m);
+            await state.setBot(message.threadID, false)
+            await api.sendMessage(m, message.threadID);
             return
         }
         if (message.body == 'switch draw') {
             const m = `Switching to draw mode.`
-            state.changeMode(message.threadID, "draw")
-            api.sendMessage(m, message.threadID);
+            await state.changeMode(message.threadID, "draw")
+            await api.sendMessage(m, message.threadID);
             return
         }
 
         if (message.body == 'stop draw') {
             const m = `Switching to chat mode.`
-            state.changeMode(message.threadID, "chat")
-            api.sendMessage(m, message.threadID);
+            await state.changeMode(message.threadID, "chat")
+            await api.sendMessage(m, message.threadID);
             return
         }
 
         if (message.body == 'bot status') {
             const m = `Bot status is ${state.getBot(message.threadID) ? "on" : "off"}.`
-            state.addConversation(message.threadID, "system", m);
-            api.sendMessage(m, message.threadID);
+            await state.addConversation(message.threadID, "system", m);
+            await api.sendMessage(m, message.threadID);
             return
         }
 
         if (message.body.split(" ").length < 3) {
-            api.sendMessage(`Too less word to answear`, message.threadID);
+            await api.sendMessage(`Too less word to answear`, message.threadID);
             return
         }
         if (state.getMode(message.threadID) == "draw") {
-            Draw(message.body).then(img => {
+            await Draw(message.body).then(img => {
                 api.sendMessage({
                     attachment: img,
                 }, message.threadID);
@@ -77,20 +77,20 @@ login({ appState: JSON.parse(CREDENTIAL) }, {}, (err, api) => {
         }
         if (state.getMode(message.threadID) == "chat") {
             const myInterval = setInterval(() => api.sendTypingIndicator(message.threadID), 1000);
-            state.addConversation(message.threadID, "user", message.body);
-            ChatGPT(state.getConversation(message.threadID), message.threadID).then(res => {
+            await state.addConversation(message.threadID, "user", message.body);
+            await ChatGPT(state.getConversation(message.threadID), message.threadID).then(async res => {
                 if (res.content.length > 2000) {
                     res.content.split("\n").map(t => api.sendMessage(t, message.threadID))
                 } else {
-                    state.addConversation(message.threadID, res.role, res.content)
-                    api.sendMessage(res.content, message.threadID);
+                    await state.addConversation(message.threadID, res.role, res.content)
+                    await api.sendMessage(res.content, message.threadID);
                 }
-            }).catch(err => {
+            }).catch(async err => {
                 if (err.message == "CHAT_GPT_INVATID_RESPONSE") {
-                    api.sendMessage("Please ask some else question that i can answer !!!", message.threadID);
+                    await api.sendMessage("Please ask some else question that i can answer !!!", message.threadID);
                 }
-                api.sendMessage("Chat GPT in stunned by unknown meteorite !!", message.threadID);
-                api.sendMessage(err, message.threadID);
+                await api.sendMessage("Chat GPT in stunned by unknown meteorite !!", message.threadID);
+                await api.sendMessage(err, message.threadID);
             }).finally(() => clearInterval(myInterval))
         }
     });
--- state.js
+++ state.js
@@ -1,3 +1,10 @@
+import { createClient } from 'redis';
+const REDIS_URL = process.env.REDIS_URL
+const client = createClient({
+    url: REDIS_URL
+});
+await client.connect();
+
 module.exports = class State {
     // [thread: string]: {
     //     status: true,
@@ -10,50 +17,27 @@ module.exports = class State {
     static MAX_CONVERSATION = 10;
     static REDUCE_RATIO = 0.5;
     setBot(threadID, status = true) {
-        if (!this[threadID]) {
-            this[threadID] = {}
-        }
-        this[threadID].status = status;
+        return client.HSET(`${threadID}::state`, "status", status);
     }
     getMode(threadID) {
-        if (!this[threadID]) {
-            this[threadID] = {}
-        }
-        return this[threadID].mode || "chat";
+        return client.HGET(`${threadID}::state`, "status").then(d => d || "chat")
     }
     changeMode(threadID, mode) {
-        if (!this[threadID]) {
-            this[threadID] = {}
-        }
-        this[threadID].mode = mode;
+        return client.HSET(`${threadID}::state`, "mode", mode);
     }
     getBot(threadID) {
-        if (!this[threadID]) {
-            this[threadID] = {}
-        }
-        return this[threadID].status
+        return client.HGET(`${threadID}::state`, "status");
     }
     getConversation(threadID) {
-        if (!this[threadID]) {
-            this[threadID] = {}
-        }
-        if (!this[threadID].conversations) {
-            this[threadID].conversations = []
-        }
-        return this[threadID].conversations;
+        return client.LRANGE(`${threadID}::conversations`, 0, -1)
     }
-    addConversation(threadID, role, content) {
-        if (!this[threadID]) {
-            this[threadID] = {}
-        }
-        if (!this[threadID].conversations) {
-            this[threadID].conversations = []
-        }
-        if (this[threadID].conversations.length > this.constructor.MAX_CONVERSATION) {
+    async addConversation(threadID, role, content) {
+        const listLength = await client.LLEN(`${threadID}::conversations`);
+        if (listLength > this.constructor.MAX_CONVERSATION) {
             // reduce 1/2 conversation;
-            this[threadID].conversations = this[threadID].conversations.slice(this[threadID].conversations.length - parseInt(this.constructor.MAX_CONVERSATION * this.constructor.REDUCE_RATIO), this[threadID].conversations.length)
+            await client.LTRIM(`${threadID}::conversations`, parseInt(listLength * this.constructor.REDUCE_RATIO), listLength)
         }
-        this[threadID].conversations.push({
+        return client.LPUSH(`${threadID}::conversations`, {
             role: role,
             content: content
         })
