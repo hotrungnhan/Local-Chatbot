175ba794c888c0b87da627c501b653b5e977d2bf
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2023-03-14 16:39:54 +0700

    update

--- state.js
+++ state.js
@@ -1,10 +1,8 @@
-const { createClient } = require("redis")
+const Redis = require("ioredis").default
 const REDIS_URL = process.env.REDIS_URL
-const client = createClient({
+const client = Redis.createClient({
     url: REDIS_URL
-});
-client.connect().then(() => console.log("redis connected !!!"));
-
+})
 module.exports = class State {
     // [thread: string]: {
     //     status: true,
@@ -17,29 +15,31 @@ module.exports = class State {
     static MAX_CONVERSATION = 10;
     static REDUCE_RATIO = 0.5;
     setBot(threadID, status = true) {
-        return client.HSET(`${threadID}::state`, "status", status);
+        return client.hset(`${threadID}::state`, JSON.stringify({
+            status: status
+        }));
     }
     getMode(threadID) {
-        return client.HGET(`${threadID}::state`, "status").then(d => d || "chat")
+        return client.hget(`${threadID}::state`, "mode").then(d => d || "chat")
     }
     changeMode(threadID, mode) {
-        return client.HSET(`${threadID}::state`, "mode", mode);
+        return client.hset(`${threadID}::state`, { "mode": mode });
     }
     getBot(threadID) {
-        return client.HGET(`${threadID}::state`, "status");
+        return client.hget(`${threadID}::state`, "status");
     }
-    getConversation(threadID) {
-        return client.LRANGE(`${threadID}::conversations`, 0, -1)
+    async getConversation(threadID) {
+        return (await client.lrange(`${threadID}::conversations`, 0, -1)).map(s => JSON.parse(s))
     }
     async addConversation(threadID, role, content) {
-        const listLength = await client.LLEN(`${threadID}::conversations`);
+        const listLength = await client.llen(`${threadID}::conversations`);
         if (listLength > this.constructor.MAX_CONVERSATION) {
             // reduce 1/2 conversation;
-            await client.LTRIM(`${threadID}::conversations`, parseInt(listLength * this.constructor.REDUCE_RATIO), listLength)
+            await client.ltrim(`${threadID}::conversations`, parseInt(listLength * this.constructor.REDUCE_RATIO), listLength)
         }
-        return client.LPUSH(`${threadID}::conversations`, {
+        return client.lpush(`${threadID}::conversations`, JSON.stringify({
             role: role,
             content: content
-        })
+        }))
     }
 }
